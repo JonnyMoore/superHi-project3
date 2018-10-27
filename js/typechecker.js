89c3cb9d5570a0fb57054561bb819ceaf1ecab86
Jonathan Moore <jonathanmoore@Jonathans-MBP-2.lan>
2018-10-27 15:19:14 +0100

    penultimate edits

--- js/typechecker.js
+++ js/typechecker.js
@@ -1,13 +1,20 @@
 
 const sentenceTag = document.querySelector(`input[type="text"]`)
 
+const typefaceTag =  document.querySelector(`select[name="typeface"]`)
+
 const typeSizeTag = document.querySelector(`input[name="typesize"]`)
 const typesizeOutputTag = document.querySelector("span.typesize-output")
+
+const fontweightTag = document.querySelector(`input[name="fontweight"]`)
+const fontweightOutputTag = document.querySelector("span.fontweight-output")
 
 const lineheightTag = document.querySelector(`input[name="lineheight"]`)
 const lineheightOutputTag = document.querySelector("span.lineheight-output")
 
 const italicTag =  document.querySelector(`input[name="italic"]`)
+
+const colorTags = document.querySelectorAll("div.colors div")
 
 const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.innerHTML
@@ -41,6 +48,13 @@ typeSizeTag.addEventListener("input", function() {
         typesizeOutputTag.innerHTML = this.value + "px"
 })
 
+// when i change my fontweight slider, update the text next to it AND
+// change the outputTag's font weight 
+fontweightTag.addEventListener("input", function() {
+    outputTag.style.fontWeight = this.value
+    fontweightOutputTag.innerHTML = this.value
+})
+
 // when i change my leading slider, update the text next to it AND
 // change the outputTag's line height
 lineheightTag.addEventListener("input", function () {
@@ -57,4 +71,26 @@ italicTag.addEventListener("change", function() {
     else {
         outputTag.style.fontStyle = "normal"
     }
+})
+
+// when i change my typeface selector, update the font-family accordingly
+typefaceTag.addEventListener("input", function () {
+    outputTag.style.fontFamily = this.value
+})
+
+// go through all of my color tags, then when i click one of them,
+//change the background color and the text color and make the tag
+// selected
+colorTags.forEach(tag => {
+    tag.addEventListener("click", function() {
+        outputTag.style.backgroundColor = this.style.backgroundColor
+        outputTag.style.color = this.style.color
+        outputTag.style.textShadow = this.style.textShadow
+
+        // reset the classes
+        colorTags.forEach(tag => {
+            tag.classList.remove("selected")
+        })
+        this.classList.add("selected")
+    })
 })
